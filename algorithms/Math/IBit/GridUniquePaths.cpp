9b0f029063c04bbf1d26a232a59e3d8c20cb2227
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-03-16 20:07:32 +0530

    Update GridUniquePaths.cpp

--- algorithms/Math/IBit/GridUniquePaths.cpp
+++ algorithms/Math/IBit/GridUniquePaths.cpp
@@ -35,3 +35,16 @@ int Solution::uniquePaths(int A, int B) {
   }
   return dp[A-1][B-1];
 }
+//Editorial Solution:
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+    // m+n-2 C n-1 = (m+n-2)! / (n-1)! (m-1)! 
+    long long ans = 1;
+    for (int i = n; i < (m + n - 1); i++) {
+      ans *= i;
+      ans /= (i - n + 1);
+    }
+    return (int)ans;
+  }
+};
