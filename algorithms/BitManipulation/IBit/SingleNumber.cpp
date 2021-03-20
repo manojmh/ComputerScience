eed8b939504850de5b32516f7c2e7218dbd5018e
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-03-20 23:13:34 +0530

    Update SingleNumber.cpp

--- algorithms/BitManipulation/IBit/SingleNumber.cpp
+++ algorithms/BitManipulation/IBit/SingleNumber.cpp
@@ -1,4 +1,4 @@
-/**
+/** Amazon
 Given an array of integers, every element appears twice except for one. Find that single one.
 Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 Input Format: First and only argument of input contains an integer array A
