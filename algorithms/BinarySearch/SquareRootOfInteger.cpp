2c9b58e7b63d22adecaf7c6400c5e6ee19655cc1
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-04-03 21:31:31 +0530

    Facebook Amazon Microsoft

--- algorithms/BinarySearch/SquareRootOfInteger.cpp
+++ algorithms/BinarySearch/SquareRootOfInteger.cpp
@@ -1,4 +1,5 @@
 /**
+https://www.interviewbit.com/problems/square-root-of-integer/
 Given an integar A.
 Compute and return the square root of A.
 If A is not a perfect square, return floor(sqrt(A)).
