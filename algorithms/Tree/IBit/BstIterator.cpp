14412c994e20620af64399d9444a24879625221b
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-06-13 12:24:24 +0530

    Apple Amazon Facebook

--- algorithms/Tree/IBit/BstIterator.cpp
+++ algorithms/Tree/IBit/BstIterator.cpp
@@ -32,3 +32,29 @@ int BSTIterator::next() {
   }
   return 0;
 }
+//Editorial
+class BSTIterator {
+  public:
+    stack<TreeNode *> myStack;
+    BSTIterator(TreeNode *root) {
+      pushAll(root);
+    }
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+      return !myStack.empty();
+    }
+    /** @return the next smallest number */
+    int next() {
+      TreeNode *tmpNode = myStack.top();
+      myStack.pop();
+      pushAll(tmpNode->right);
+      return tmpNode->val;
+    }
+  private:
+  void pushAll(TreeNode *node) {
+    while (node != NULL) {
+      myStack.push(node);
+      node = node->left;
+    }
+  }
+};
