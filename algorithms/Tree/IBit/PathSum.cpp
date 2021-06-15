70fdf75132ef536a01fdf70fd436b1e9ea0205d7
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-06-16 02:04:58 +0530

    Microsoft Yahoo Amazon

---
+++ algorithms/Tree/IBit/PathSum.cpp
@@ -0,0 +1,21 @@
+/**
+https://www.interviewbit.com/problems/path-sum/
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+Example :
+Given the below binary tree and sum = 22,
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+**/
+int Solution::hasPathSum(TreeNode* A, int B) {
+  if(!A) return false;
+  if(A->left == nullptr && A->right == nullptr && (B-A->val)==0)
+    return true;
+  return (hasPathSum(A->left, B-A->val) || hasPathSum(A->right, B-A->val));
+}
