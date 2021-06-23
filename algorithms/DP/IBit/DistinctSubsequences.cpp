8b674ae9a3df979bdd052f3d4f93cccb478f6df7
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-06-23 23:16:20 +0530

    Google 2d String DP

--- algorithms/DP/IBit/DistinctSubsequences.cpp
+++ algorithms/DP/IBit/DistinctSubsequences.cpp
@@ -24,7 +24,31 @@ These are the possible removals of characters:
 => A = "rab_bit" 
 => A = "rabb_it"
 Note: "_" marks the removed character.
+Intution: https://www.youtube.com/watch?v=-RDzMJ33nx8
 **/
+int Solution::numDistinct(string A, string B) {
+  int m = A.size();
+  int n = B.size();
+  int t[m+1][n+1];
+  memset(t, 0, sizeof(t));
+  for(int i=0; i<=m; i++)
+    t[i][0] = 1;
+  for(int i=1; i<=m; i++) {
+    for(int j=1; j<=n; j++) {
+      if(A[i-1]==B[j-1]) {
+        //char at A is matching with B
+        //so we are considering the char at A hence t[i-1][j-1]
+        //but there might be one more char at A
+        //which is matching with B's current char hence t[i-1][j]
+        t[i][j] = t[i-1][j-1] + t[i-1][j];
+      }
+      else
+        t[i][j] = t[i-1][j];
+    }
+  }
+  return t[m][n];
+}
+//Another Solution
 int Solution::numDistinct(string A, string B) {
   int m = A.size();
   int n = B.size();
