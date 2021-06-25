f40177a0e49eaf71b3a422bee6e6b225f9659b10
Manoj Hanamshet <manoj.bgm@gmail.com>
2021-06-25 23:52:27 +0530

    Amazon Facebook

--- algorithms/DP/IBit/BestTimeToBuyAndSellStocksII.cpp
+++ algorithms/DP/IBit/BestTimeToBuyAndSellStocksII.cpp
@@ -30,6 +30,19 @@ Intution:
 Update buy through out downtrend and keep resetting sell. At every start of down trend check for sell!=0 and take profit
 Update sell through out the uptrend
 **/
+//Editorial:
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+    int total = 0, sz = prices.size();
+    for(int i = 0; i < sz - 1; i++) {
+      if(prices[i+1] > prices[i])
+        total += prices[i+1] - prices[i];
+    }
+    return total;
+  }
+};
+//My Solution:
 int Solution::maxProfit(const vector<int> &A) {
   int buy = 0;
   int sell = 0;
