6e231a8f3beac9035452e2aef433223913936ab7
Manoj Hanamshet <manoj.bgm@gmail.com>
2022-05-22 12:29:28 +0530

    Update BalancedParentheses.cpp

--- GrokkingTheCodingInterview/11.PatternSubsets/6.BalancedParentheses/BalancedParentheses.cpp
+++ GrokkingTheCodingInterview/11.PatternSubsets/6.BalancedParentheses/BalancedParentheses.cpp
@@ -10,21 +10,20 @@ Output: ((())), (()()), (())(), ()(()), ()()()
 */
 vector<string> BalancedParanthesis(int num) {
   if(num < 1) return vector<string>();
-
-	vector<string> res;
-	queue<ParaString> q;
-	q.push({"", 0, 0});
-	while(!q.empty()) {
+  vector<string> res;
+  queue<ParaString> q;
+  q.push({"", 0, 0});
+  while(!q.empty()) {
     ParaString ps(q.front());
-		q.pop();
-		if(ps.open == num && ps.close == num)
-			res.push_back(ps.str);
-		else {
-			if(ps.open<num)
-				q.push({ps.str+"(", ps.open+1, ps.close});
-			if(ps.open > ps.close)
-				q.push({ps.str+")", ps.open, ps.close+1});
-		}
-	}
-	return res;
+    q.pop();
+    if(ps.open == num && ps.close == num)
+      res.push_back(ps.str);
+    else {
+      if(ps.open<num)
+        q.push({ps.str+"(", ps.open+1, ps.close});
+      if(ps.open > ps.close)
+        q.push({ps.str+")", ps.open, ps.close+1});
+    }
+  }
+  return res;
 }
